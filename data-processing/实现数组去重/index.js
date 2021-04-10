1ca3d0e67ac91b236c7060ae274e526ff41e914e
ppcia <ppcia@icloud.com>
2021-04-10 10:55:20 +0800

    update:数组去重

--- data-processing/实现数组去重/index.js
+++ data-processing/实现数组去重/index.js
@@ -8,8 +8,10 @@ Array.from(new Set(arr))
 /* 
     ES5:使用map存储不重复的数字
 */
-function uniqueArray(arr){
+function uniqueArray1(arr){
+    // 声明一个空对象，用于存放数组中的下标
     let map = {}
+    // 声明一个空数组
     let res = []
     for(var i= 0; i< arr.length; i++){
         if(!map.hasOwnProperty([arr[i]])){
@@ -20,3 +22,20 @@ function uniqueArray(arr){
     return res
 }
 
+/* 
+    利用forEech()和indexOf()
+    本质上是双重遍历，效率差些
+*/
+function uniqueArray2(arr){
+    //声明一个空数组
+    const result = []
+    // 遍历原始数组
+    arr.forEach(item=>{
+        // 检测result数组中是否包含这个元素
+        if(result.indexOf(item) === -1){
+            // 如果没有该元素
+            result.push(item)
+        }
+    })
+    return result
+}
